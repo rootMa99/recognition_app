eb4dcd3ac4ea2f7c161ae5c8feafb50cddabfa7b
rootMa99 <anasszeroual09@gmail.com>
2024-06-27 19:51:40 +0100

    sd

---
+++ src/DemoData.js
@@ -0,0 +1,6 @@
+export const USERS = [
+  { role: "fa", userName: "fa", token: "xxx.yyy.zzz"},
+  { role: "cutting", userName: "cutting", token: "xxx.yyy.zzz" },
+  { role: "root", userName: "root", token: "xxx.yyy.zzz" },
+];
+
